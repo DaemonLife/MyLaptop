00c2413766d7e8600a3ac03c699374fd931663ab
DaemonLife <52444457+DaemonLife@users.noreply.github.com>
2022-05-07 22:59:21 +0300

    Delete block_screen.sh

--- .config/sway/block_screen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-swayidle -w \
-	timeout 3 'swaylock -f -c 000000' \
-	timeout 6 'swaymsg "output * dpms off"' \
-		resume 'swaymsg "output * dpms on"' \
-	before-sleep 'swaylock -f -c 000000'
